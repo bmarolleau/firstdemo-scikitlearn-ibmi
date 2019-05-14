85a939f3351947e1770093251026a03bbe1d66b9
bmarolleau <benoit.marolleau@fr.ibm.Commmmmmmmmm>
2019-05-14 12:29:37 +0200

    first commit

---
+++ 0_download_Dataset_CSV.sh
@@ -0,0 +1,2 @@
+wget https://community.watsonanalytics.com/wp-content/uploads/2015/03/WA_Fn-UseC_-Telco-Customer-Churn.csv  --no-check-certificate
+
---
+++ 16_load_SVC_ClassifierModel.py
@@ -0,0 +1,4 @@
+
+from joblib import load
+clf_svc = load('./SVC_Model_CHURN_IBMi_V1.joblib') 
+
